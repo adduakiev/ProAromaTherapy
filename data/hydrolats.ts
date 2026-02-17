d1ce3879ddeca8f4a1db5033fbe3c4eee38fad5f
adduakiev <adduakiev@gmail.com>
2026-02-17 22:45:47 +0000

    restore: full database with glass bottle support

--- data/hydrolats.ts
+++ data/hydrolats.ts
@@ -1,14 +1,565 @@
 import { Product } from "../types";
 
 export const hydrolats: Product[] = [
-  { id: "GL-126", code: "GL-126", name: "Аїр болотний", latinName: "Acorus calamus", type: "hydrolat", rawMaterial: "водний дистилят", purchasePriceEurPerKg: 4.5, retailPrices: [{ volume: 100, price: 170 }, { volume: 101, price: 220 }, { volume: 200, price: 280 }, { volume: 500, price: 610 }] },
-  { id: "GL-170", code: "GL-170", name: "Акація біла", latinName: "Robinia pseudoacacia", type: "hydrolat", rawMaterial: "водний дистилят", purchasePriceEurPerKg: 5.1, retailPrices: [{ volume: 100, price: 200 }, { volume: 101, price: 240 }, { volume: 200, price: 300 }, { volume: 500, price: 640 }] },
-  { id: "GL-180", code: "GL-180", name: "Алое деревоподібне", latinName: "Aloe arborescens", type: "hydrolat", rawMaterial: "водний дистилят", purchasePriceEurPerKg: 9.6, retailPrices: [{ volume: 100, price: 300 }, { volume: 101, price: 350 }, { volume: 200, price: 470 }, { volume: 500, price: 1020 }] },
-  { id: "GL-107", code: "GL-107", name: "Апельсин солодкий", latinName: "Citrus sinensis", type: "hydrolat", rawMaterial: "водний дистилят", purchasePriceEurPerKg: 4.0, retailPrices: [{ volume: 100, price: 150 }, { volume: 101, price: 200 }, { volume: 200, price: 240 }, { volume: 500, price: 520 }] },
-  { id: "GL-187", code: "GL-187", name: "Безсмертник італійський", latinName: "Helichrysum italicum", type: "hydrolat", rawMaterial: "водний дистилят", purchasePriceEurPerKg: 6.0, retailPrices: [{ volume: 100, price: 230 }, { volume: 101, price: 280 }, { volume: 200, price: 370 }, { volume: 500, price: 790 }] },
-  { id: "GL-174", code: "GL-174", name: "Волошка синя", latinName: "Centaurea cyanus", type: "hydrolat", rawMaterial: "водний дистилят", purchasePriceEurPerKg: 5.7, retailPrices: [{ volume: 100, price: 220 }, { volume: 101, price: 260 }, { volume: 200, price: 350 }, { volume: 500, price: 740 }] },
-  { id: "GL-141", code: "GL-141", name: "Лаванда вузьколиста", latinName: "Lavandula angustifolia", type: "hydrolat", rawMaterial: "водний дистилят", purchasePriceEurPerKg: 7.6, retailPrices: [{ volume: 100, price: 200 }, { volume: 101, price: 250 }, { volume: 200, price: 350 }, { volume: 500, price: 740 }] },
-  { id: "GL-122", code: "GL-122", name: "Ромашка аптечна", latinName: "Matricaria chamomilla", type: "hydrolat", rawMaterial: "водний дистилят", purchasePriceEurPerKg: 4.0, retailPrices: [{ volume: 100, price: 150 }, { volume: 101, price: 200 }, { volume: 200, price: 240 }, { volume: 500, price: 520 }] },
-  { id: "GL-164", code: "GL-164", name: "Ісоп лікарський", latinName: "Hyssopus officinalis", type: "hydrolat", rawMaterial: "водний дистилят", purchasePriceEurPerKg: 4.5, retailPrices: [{ volume: 100, price: 200 }, { volume: 101, price: 240 }, { volume: 200, price: 300 }, { volume: 500, price: 640 }] }
+  {
+    id: "GL-126",
+    code: "GL-126",
+    name: "Аїр болотний",
+    nameRu: "Аир болотный",
+    latinName: "Acorus calamus",
+    keywords: "Аїр болотний Аир болотный Acorus calamus гідролат",
+    type: "hydrolat",
+    rawMaterial: "водний дистилят",
+    purchasePriceEurPerKg: 4.5,
+    retailPrices: [
+      { volume: 100, price: 170 },
+      { volume: 101, price: 220 },
+      { volume: 200, price: 280 },
+      { volume: 500, price: 610 }
+    ]
+  },
+  {
+    id: "GL-170",
+    code: "GL-170",
+    name: "Акація біла",
+    nameRu: "Акация белая",
+    latinName: "Robinia pseudoacacia",
+    keywords: "Акація біла Акация белая Robinia pseudoacacia гідролат",
+    type: "hydrolat",
+    rawMaterial: "водний дистилят",
+    purchasePriceEurPerKg: 5.1,
+    retailPrices: [
+      { volume: 100, price: 200 },
+      { volume: 101, price: 240 },
+      { volume: 200, price: 300 },
+      { volume: 500, price: 640 }
+    ]
+  },
+  {
+    id: "GL-180",
+    code: "GL-180",
+    name: "Алое деревоподібне",
+    nameRu: "Алоэ древовидное",
+    latinName: "Aloe arborescens",
+    keywords: "Алое деревоподібне Алоэ древовидное Aloe arborescens гідролат",
+    type: "hydrolat",
+    rawMaterial: "водний дистилят",
+    purchasePriceEurPerKg: 9.6,
+    retailPrices: [
+      { volume: 100, price: 300 },
+      { volume: 101, price: 350 },
+      { volume: 200, price: 470 },
+      { volume: 500, price: 1020 }
+    ]
+  },
+  {
+    id: "GL-107",
+    code: "GL-107",
+    name: "Апельсин солодкий",
+    nameRu: "Апельсин сладкий",
+    latinName: "Citrus sinensis",
+    keywords: "Апельсин солодкий Апельсин сладкий Citrus sinensis гідролат",
+    type: "hydrolat",
+    rawMaterial: "водний дистилят",
+    purchasePriceEurPerKg: 4.0,
+    retailPrices: [
+      { volume: 100, price: 150 },
+      { volume: 101, price: 200 },
+      { volume: 200, price: 240 },
+      { volume: 500, price: 520 }
+    ]
+  },
+  {
+    id: "GL-187",
+    code: "GL-187",
+    name: "Безсмертник італійський",
+    nameRu: "Бессмертник итальянский",
+    latinName: "Helichrysum italicum",
+    keywords: "Безсмертник італійський Бессмертник итальянский Helichrysum italicum гідролат",
+    type: "hydrolat",
+    rawMaterial: "водний дистилят",
+    purchasePriceEurPerKg: 6.0,
+    retailPrices: [
+      { volume: 100, price: 230 },
+      { volume: 101, price: 280 },
+      { volume: 200, price: 370 },
+      { volume: 500, price: 790 }
+    ]
+  },
+  {
+    id: "GL-188",
+    code: "GL-188",
+    name: "Берези повислої бруньки",
+    nameRu: "Берёза повислая(почки)",
+    latinName: "Betula pendula",
+    keywords: "Берези повислої бруньки Берёза повислая(почки) Betula pendula гідролат",
+    type: "hydrolat",
+    rawMaterial: "водний дистилят",
+    purchasePriceEurPerKg: 6.0,
+    retailPrices: [
+      { volume: 100, price: 230 },
+      { volume: 101, price: 280 },
+      { volume: 200, price: 370 },
+      { volume: 500, price: 790 }
+    ]
+  },
+  {
+    id: "GL-174",
+    code: "GL-174",
+    name: "Волошка синя",
+    nameRu: "Василёк синий",
+    latinName: "Centaurea cyanus",
+    keywords: "Волошка синя Василёк синий Centaurea cyanus гідролат",
+    type: "hydrolat",
+    rawMaterial: "водний дистилят",
+    purchasePriceEurPerKg: 5.7,
+    retailPrices: [
+      { volume: 100, price: 220 },
+      { volume: 101, price: 260 },
+      { volume: 200, price: 350 },
+      { volume: 500, price: 740 }
+    ]
+  },
+  {
+    id: "GL-109",
+    code: "GL-109",
+    name: "Грейфрут",
+    nameRu: "Грейфрут",
+    latinName: "Citrus paradisi",
+    keywords: "Грейфрут Грейфрут Citrus paradisi гідролат",
+    type: "hydrolat",
+    rawMaterial: "водний дистилят",
+    purchasePriceEurPerKg: 4.0,
+    retailPrices: [
+      { volume: 100, price: 150 },
+      { volume: 101, price: 200 },
+      { volume: 200, price: 240 },
+      { volume: 500, price: 520 }
+    ]
+  },
+  {
+    id: "GL-102",
+    code: "GL-102",
+    name: "Деревій звичайний",
+    nameRu: "Тысячелистник обыкновенный",
+    latinName: "Achillea millefoluim",
+    keywords: "Деревій звичайний Тысячелистник обыкновенный Achillea millefoluim гідролат",
+    type: "hydrolat",
+    rawMaterial: "водний дистилят",
+    purchasePriceEurPerKg: 4.5,
+    retailPrices: [
+      { volume: 100, price: 150 },
+      { volume: 101, price: 200 },
+      { volume: 200, price: 240 },
+      { volume: 500, price: 520 }
+    ]
+  },
+  {
+    id: "GL-103",
+    code: "GL-103",
+    name: "Деревій щетинистий",
+    nameRu: "Тысячелистник щетинистый",
+    latinName: "Achillеa setаcea",
+    keywords: "Деревій щетинистий Тысячелистник щетинистый Achillеa setаcea гідролат",
+    type: "hydrolat",
+    rawMaterial: "водний дистилят",
+    purchasePriceEurPerKg: 4.5,
+    retailPrices: [
+      { volume: 100, price: 150 },
+      { volume: 101, price: 200 },
+      { volume: 200, price: 240 },
+      { volume: 500, price: 520 }
+    ]
+  },
+  {
+    id: "GL-163",
+    code: "GL-163",
+    name: "Евкаліпт прутоподібний",
+    nameRu: "Эвкалипт прутовидный",
+    latinName: "Eucalyptus viminаlis",
+    keywords: "Евкаліпт прутоподібний Эвкалипт прутовидный Eucalyptus viminаlis гідролат",
+    type: "hydrolat",
+    rawMaterial: "водний дистилят",
+    purchasePriceEurPerKg: 4.5,
+    retailPrices: [
+      { volume: 100, price: 170 },
+      { volume: 101, price: 220 },
+      { volume: 200, price: 280 },
+      { volume: 500, price: 610 }
+    ]
+  },
+  {
+    id: "GL-134",
+    code: "GL-134",
+    name: "Звіробій продірявлений",
+    nameRu: "Зверобой продырявленный",
+    latinName: "Hypericum perforatum",
+    keywords: "Звіробій продірявлений Зверобой продырявленный Hypericum perforatum гідролат",
+    type: "hydrolat",
+    rawMaterial: "водний дистилят",
+    purchasePriceEurPerKg: 4.5,
+    retailPrices: [
+      { volume: 100, price: 170 },
+      { volume: 101, price: 220 },
+      { volume: 200, price: 280 },
+      { volume: 500, price: 610 }
+    ]
+  },
+  {
+    id: "GL-164",
+    code: "GL-164",
+    name: "Ісоп лікарський",
+    nameRu: "Иссоп лекарственный",
+    latinName: "Hyssopus officinalis",
+    keywords: "Ісоп лікарський Иссоп лекарственный Hyssopus officinalis гідролат",
+    type: "hydrolat",
+    rawMaterial: "водний дистилят",
+    purchasePriceEurPerKg: 4.5,
+    retailPrices: [
+      { volume: 100, price: 200 },
+      { volume: 101, price: 240 },
+      { volume: 200, price: 300 },
+      { volume: 500, price: 640 }
+    ]
+  },
+  {
+    id: "GL-136",
+    code: "GL-136",
+    name: "Календула лікарська",
+    nameRu: "Календула лекарственная",
+    latinName: "Calendula officinalis",
+    keywords: "Календула лікарська Календула лекарственная Calendula officinalis гідролат",
+    type: "hydrolat",
+    rawMaterial: "водний дистилят",
+    purchasePriceEurPerKg: 4.8,
+    retailPrices: [
+      { volume: 100, price: 180 },
+      { volume: 101, price: 230 },
+      { volume: 200, price: 320 },
+      { volume: 500, price: 690 }
+    ]
+  },
+  {
+    id: "GL-138",
+    code: "GL-138",
+    name: "Коріандр посівний (насіння)",
+    nameRu: "Кориандр посевной(семена)",
+    latinName: "Coriandrum sativum",
+    keywords: "Коріандр посівний (насіння) Кориандр посевной(семена) Coriandrum sativum гідролат",
+    type: "hydrolat",
+    rawMaterial: "водний дистилят",
+    purchasePriceEurPerKg: 4.0,
+    retailPrices: [
+      { volume: 100, price: 160 },
+      { volume: 101, price: 210 },
+      { volume: 200, price: 260 },
+      { volume: 500, price: 550 }
+    ]
+  },
+  {
+    id: "GL-140",
+    code: "GL-140",
+    name: "Кропива дводомна",
+    nameRu: "Крапива двудомная",
+    latinName: "Urtica diоica",
+    keywords: "Кропива дводомна Крапива двудомная Urtica diоica гідролат",
+    type: "hydrolat",
+    rawMaterial: "водний дистилят",
+    purchasePriceEurPerKg: 3.9,
+    retailPrices: [
+      { volume: 100, price: 150 },
+      { volume: 101, price: 200 },
+      { volume: 200, price: 240 },
+      { volume: 500, price: 520 }
+    ]
+  },
+  {
+    id: "GL-141",
+    code: "GL-141",
+    name: "Лаванда вузьколиста",
+    nameRu: "Лаванда узколистная",
+    latinName: "Lavandula angustifolia",
+    keywords: "Лаванда вузьколиста Лаванда узколистная Lavandula angustifolia гідролат",
+    type: "hydrolat",
+    rawMaterial: "водний дистилят",
+    purchasePriceEurPerKg: 7.6,
+    retailPrices: [
+      { volume: 100, price: 200 },
+      { volume: 101, price: 250 },
+      { volume: 200, price: 350 },
+      { volume: 500, price: 740 }
+    ]
+  },
+  {
+    id: "GL-110",
+    code: "GL-110",
+    name: "Лимон",
+    nameRu: "Лимон",
+    latinName: "Citrus limon",
+    keywords: "Лимон Лимон Citrus limon гідролат",
+    type: "hydrolat",
+    rawMaterial: "водний дистилят",
+    purchasePriceEurPerKg: 4.0,
+    retailPrices: [
+      { volume: 100, price: 150 },
+      { volume: 101, price: 200 },
+      { volume: 200, price: 240 },
+      { volume: 500, price: 520 }
+    ]
+  },
+  {
+    id: "GL-142",
+    code: "GL-142",
+    name: "Лофант анісовий",
+    nameRu: "Лофант анисовый",
+    latinName: "Agastache foeniculum",
+    keywords: "Лофант анісовий Лофант анисовый Agastache foeniculum гідролат",
+    type: "hydrolat",
+    rawMaterial: "водний дистилят",
+    purchasePriceEurPerKg: 4.5,
+    retailPrices: [
+      { volume: 100, price: 170 },
+      { volume: 101, price: 220 },
+      { volume: 200, price: 280 },
+      { volume: 500, price: 610 }
+    ]
+  },
+  {
+    id: "GL-143",
+    code: "GL-143",
+    name: "Любисток лікарський (коріння)",
+    nameRu: "Любисток лекарственный(корни)",
+    latinName: "Levisticum officinаle",
+    keywords: "Любисток лікарський (коріння) Любисток лекарственный(корни) Levisticum officinаle гідролат",
+    type: "hydrolat",
+    rawMaterial: "водний дистилят",
+    purchasePriceEurPerKg: 4.5,
+    retailPrices: [
+      { volume: 100, price: 170 },
+      { volume: 101, price: 220 },
+      { volume: 200, price: 280 },
+      { volume: 500, price: 610 }
+    ]
+  },
+  {
+    id: "GL-111",
+    code: "GL-111",
+    name: "Мандарин",
+    nameRu: "Мандарин",
+    latinName: "Citrus reticulata",
+    keywords: "Мандарин Мандарин Citrus reticulata гідролат",
+    type: "hydrolat",
+    rawMaterial: "водний дистилят",
+    purchasePriceEurPerKg: 4.0,
+    retailPrices: [
+      { volume: 100, price: 150 },
+      { volume: 101, price: 200 },
+      { volume: 200, price: 240 },
+      { volume: 500, price: 520 }
+    ]
+  },
+  {
+    id: "GL-144",
+    code: "GL-144",
+    name: "Монарда дудчаста",
+    nameRu: "Монарда дудчатая",
+    latinName: "Monarda fistulosa",
+    keywords: "Монарда дудчаста Монарда дудчатая Monarda fistulosa гідролат",
+    type: "hydrolat",
+    rawMaterial: "водний дистилят",
+    purchasePriceEurPerKg: 4.8,
+    retailPrices: [
+      { volume: 100, price: 170 },
+      { volume: 101, price: 220 },
+      { volume: 200, price: 280 },
+      { volume: 500, price: 610 }
+    ]
+  },
+  {
+    id: "GL-145",
+    code: "GL-145",
+    name: "Морква дика (насіння)",
+    nameRu: "Морковь дикая(семена)",
+    latinName: "Daucus carota",
+    keywords: "Морква дика (насіння) Морковь дикая(семена) Daucus carota гідролат",
+    type: "hydrolat",
+    rawMaterial: "водний дистилят",
+    purchasePriceEurPerKg: 4.5,
+    retailPrices: [
+      { volume: 100, price: 170 },
+      { volume: 101, price: 220 },
+      { volume: 200, price: 280 },
+      { volume: 500, price: 610 }
+    ]
+  },
+  {
+    id: "GL-151",
+    code: "GL-151",
+    name: "Петрушка звичайна (насіння)",
+    nameRu: "Петрушка обыкновенная(семена)",
+    latinName: "Petroselinum crispum",
+    keywords: "Петрушка звичайна (насіння) Петрушка обыкновенная(семена) Petroselinum crispum гідролат",
+    type: "hydrolat",
+    rawMaterial: "водний дистилят",
+    purchasePriceEurPerKg: 4.2,
+    retailPrices: [
+      { volume: 100, price: 160 },
+      { volume: 101, price: 210 },
+      { volume: 200, price: 260 },
+      { volume: 500, price: 550 }
+    ]
+  },
+  {
+    id: "GL-185",
+    code: "GL-185",
+    name: "Півонія молочноквіткова",
+    nameRu: "Пион молочноцветковый",
+    latinName: "Paeonia lactiflora",
+    keywords: "Півонія молочноквіткова Пион молочноцветковый Paeonia lactiflora гідролат",
+    type: "hydrolat",
+    rawMaterial: "водний дистилят",
+    purchasePriceEurPerKg: 4.5,
+    retailPrices: [
+      { volume: 100, price: 170 },
+      { volume: 101, price: 220 },
+      { volume: 200, price: 280 },
+      { volume: 500, price: 610 }
+    ]
+  },
+  {
+    id: "GL-152",
+    code: "GL-152",
+    name: "Пімента лікарська",
+    nameRu: "Пимента лекарственная",
+    latinName: "Pimenta officinalis",
+    keywords: "Пімента лікарська Пимента лекарственная Pimenta officinalis гідролат",
+    type: "hydrolat",
+    rawMaterial: "водний дистилят",
+    purchasePriceEurPerKg: 4.5,
+    retailPrices: [
+      { volume: 100, price: 170 },
+      { volume: 101, price: 220 },
+      { volume: 200, price: 280 },
+      { volume: 500, price: 610 }
+    ]
+  },
+  {
+    id: "GL-116",
+    code: "GL-116",
+    name: "Полин австрійський",
+    nameRu: "Полынь австрийская",
+    latinName: "Artemisia austriaca",
+    keywords: "Полин австрійський Полынь австрийская Artemisia austriaca гідролат",
+    type: "hydrolat",
+    rawMaterial: "водний дистилят",
+    purchasePriceEurPerKg: 3.9,
+    retailPrices: [
+      { volume: 100, price: 150 },
+      { volume: 101, price: 200 },
+      { volume: 200, price: 240 },
+      { volume: 500, price: 520 }
+    ]
+  },
+  {
+    id: "GL-117",
+    code: "GL-117",
+    name: "Полин гіркий",
+    nameRu: "Полынь горькая",
+    latinName: "Artemisia absinthium",
+    keywords: "Полин гіркий Полынь горькая Artemisia absinthium гідролат",
+    type: "hydrolat",
+    rawMaterial: "водний дистилят",
+    purchasePriceEurPerKg: 4.0,
+    retailPrices: [
+      { volume: 100, price: 150 },
+      { volume: 101, price: 200 },
+      { volume: 200, price: 240 },
+      { volume: 500, price: 520 }
+    ]
+  },
+  {
+    id: "GL-119",
+    code: "GL-119",
+    name: "Полин лікувальний",
+    nameRu: "Полынь лечебная",
+    latinName: "Artemisia abrotanum",
+    keywords: "Полин лікувальний Полынь лечебная Artemisia abrotanum гідролат",
+    type: "hydrolat",
+    rawMaterial: "водний дистилят",
+    purchasePriceEurPerKg: 3.8,
+    retailPrices: [
+      { volume: 100, price: 150 },
+      { volume: 101, price: 200 },
+      { volume: 200, price: 240 },
+      { volume: 500, price: 520 }
+    ]
+  },
+  {
+    id: "GL-118",
+    code: "GL-118",
+    name: "Полин однорічний",
+    nameRu: "Полынь однолетняя",
+    latinName: "Artemisia annua",
+    keywords: "Полин однорічний Полынь однолетняя Artemisia annua гідролат",
+    type: "hydrolat",
+    rawMaterial: "водний дистилят",
+    purchasePriceEurPerKg: 3.8,
+    retailPrices: [
+      { volume: 100, price: 150 },
+      { volume: 101, price: 200 },
+      { volume: 200, price: 240 },
+      { volume: 500, price: 520 }
+    ]
+  },
+  {
+    id: "GL-120",
+    code: "GL-120",
+    name: "Полин сантонінний",
+    nameRu: "Полынь сантонинная",
+    latinName: "Artemisia santonica",
+    keywords: "Полин сантонінний Полынь сантонинная Artemisia santonica гідролат",
+    type: "hydrolat",
+    rawMaterial: "водний дистилят",
+    purchasePriceEurPerKg: 4.0,
+    retailPrices: [
+      { volume: 100, price: 150 },
+      { volume: 101, price: 200 },
+      { volume: 200, price: 240 },
+      { volume: 500, price: 520 }
+    ]
+  },
+  {
+    id: "GL-122",
+    code: "GL-122",
+    name: "Ромашка аптечна",
+    nameRu: "Matricaria chamomilla",
+    latinName: "Matricaria chamomilla",
+    keywords: "Ромашка аптечна Matricaria chamomilla гідролат",
+    type: "hydrolat",
+    rawMaterial: "водний дистилят",
+    purchasePriceEurPerKg: 4.0,
+    retailPrices: [
+      { volume: 100, price: 150 },
+      { volume: 101, price: 200 },
+      { volume: 200, price: 240 },
+      { volume: 500, price: 520 }
+    ]
+  },
+  {
+    id: "GL-171",
+    code: "GL-171",
+    name: "Ромашка римська",
+    nameRu: "Ромашка римская",
+    latinName: "Chamaemelum nobile",
+    keywords: "Ромашка римська Ромашка римская Chamaemelum nobile гідролат",
+    type: "hydrolat",
+    rawMaterial: "водний дистилят",
+    purchasePriceEurPerKg: 6.0,
+    retailPrices: [
+      { volume: 100, price: 230 },
+      { volume: 101, price: 280 },
+      { volume: 200, price: 370 },
+      { volume: 500, price: 790 }
+    ]
+  }
 ];
-// Для інших гідролатів просто копіюй структуру вище
