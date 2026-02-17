18080e00988d7631e09309c290d7404aa0b080ab
adduakiev <adduakiev@gmail.com>
2026-02-17 21:48:14 +0000

    refactor: split data and add glass support

--- index.tsx
+++ index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App'; // Перевір, щоб шлях був саме таким
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
